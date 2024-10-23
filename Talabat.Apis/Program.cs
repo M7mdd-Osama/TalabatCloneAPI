f0014dcbafaab4ffdc603632bcd9096062499428
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-23 06:48:59 +0300

    Done

--- Talabat.Apis/Program.cs
+++ Talabat.Apis/Program.cs
@@ -1,19 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+using Talabat.Repository;
+using Talabat.Repository.Data;
+
 namespace Talabat.Apis
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
+			#region Configure Services Add services to the container.
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+			builder.Services.AddDbContext<StoreContext>(Options =>
+			{
+				Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
+			//builder.Services.AddScoped<IGenericRepository<Product> , GenericRepository<Product>>();
+
+			builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+			#endregion
 
 			var app = builder.Build();
+
+			#region Update-Database
+			using var Scope = app.Services.CreateScope();
+			var Services = Scope.ServiceProvider;
+			var LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				var dbContext = Services.GetRequiredService<StoreContext>();
+				await dbContext.Database.MigrateAsync();
+				await StoreContextSeed.SeedAsync(dbContext);
+			}
+			catch (Exception ex)
+			{
+				var Logger = LoggerFactory.CreateLogger<Program>();
+				Logger.LogError(ex, "An Error Occured During Appling The Migration");
+			}
+			#endregion
+
+
+			#region Configure - Configure the HTTP request pipeline.                                                    
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
@@ -28,6 +62,9 @@ namespace Talabat.Apis
 
 
 			app.MapControllers();
+
+
+			#endregion
 
 			app.Run();
 		}
--- Talabat.Core/Entities/Product.cs
+++ Talabat.Core/Entities/Product.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace Talabat.Core.Entities
 {
-	internal class	Product : BaseEntity
+	public class Product : BaseEntity
 	{
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public string PictureUrl { get; set; }
-        public decimal Price { get; set; }
-        public int ProductBrandId { get; set; } //FK
-        public ProductBrand ProductBrand { get; set; }
-        public int ProductTypeId { get; set; } //FK
-        public ProductType ProductType { get; set; }
-    }
+		public decimal Price { get; set; }
+		public int ProductBrandId { get; set; } //FK
+		public ProductBrand ProductBrand { get; set; }
+		public int ProductTypeId { get; set; } //FK
+		public ProductType ProductType { get; set; }
+	}
 }
