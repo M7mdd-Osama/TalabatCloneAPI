9601889169e20a638e871377d5720e1722f4bcd3
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-24 18:25:01 +0300

    Specification Design Pattern

--- Talabat.Core/Repositories/IGenericRepository.cs
+++ Talabat.Core/Repositories/IGenericRepository.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
 
 namespace Talabat.Core.Repositories
 {
 	public interface IGenericRepository<T> where T : BaseEntity
 	{
+		#region Without Specifications
 		Task<IEnumerable<T>> GetAllAsync();
 		Task<T> GetByIdAsync(int id);
+		#endregion
+
+		#region  With Specifications
+		Task<IEnumerable<T>> GetAllWithSpecAsync(ISpecifications<T> Spec);
+		Task<T> GetByIdWithSpecAsync(ISpecifications<T> Spec);
+		#endregion
 
 	}
 }
