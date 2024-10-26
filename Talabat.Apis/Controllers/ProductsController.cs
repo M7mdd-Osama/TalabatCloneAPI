ea459332cf7c00c73f1aa2b19322b06bf469088e
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-26 16:41:04 +0300

    Get Types, Get Brands Endpoint

--- Talabat.Apis/Controllers/ProductsController.cs
+++ Talabat.Apis/Controllers/ProductsController.cs
@@ -13,12 +13,18 @@ namespace Talabat.Apis.Controllers
 	{
 		private readonly IGenericRepository<Product> _productRepo;
 		private readonly IMapper _mapper;
+		private readonly IGenericRepository<ProductType> _typeRepo;
+		private readonly IGenericRepository<ProductBrand> _brandRepo;
 
 		public ProductsController(IGenericRepository<Product> ProductRepo,
-			IMapper mapper)
+			IMapper mapper,
+			IGenericRepository<ProductType> TypeRepo,
+			IGenericRepository<ProductBrand> BrandRepo)
 		{
 			_productRepo = ProductRepo;
 			_mapper = mapper;
+			_typeRepo = TypeRepo;
+			_brandRepo = BrandRepo;
 		}
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
@@ -39,6 +45,19 @@ namespace Talabat.Apis.Controllers
 			if (Product is null) return NotFound(new ApiResponse(404));
 			var MappedProduct = _mapper.Map<Product, ProductToReturnDto>(Product);
 			return Ok(MappedProduct);
+		}
+
+		[HttpGet("Types")]
+		public async Task<ActionResult<IEnumerable<ProductType>>> GetTypes()
+		{
+			var Types = await _typeRepo.GetAllAsync();
+			return Ok(Types);
+		}
+		[HttpGet("Brands")]
+		public async Task<ActionResult<IEnumerable<ProductBrand>>> GetBrands()
+		{
+			var Brands = await _brandRepo.GetAllAsync();
+			return Ok(Brands);
 		}
 	}
 }
