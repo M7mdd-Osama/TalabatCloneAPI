b6f4d49e17cb4b1f9f5d558d839c75c80c86738d
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-27 19:05:28 +0300

    Login Endpoint

---
+++ Talabat.Apis/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Apis.DTOs;
+using Talabat.Apis.Errors;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Apis.Controllers
+{
+	public class AccountsController : APIBaseController
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AccountsController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost("Register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var User = new AppUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				UserName = model.Email.Split('@')[0],
+				PhoneNumber = model.PhoneNumber
+			};
+			var Result = await _userManager.CreateAsync(User, model.Password);
+			if (!Result.Succeeded) return BadRequest(new ApiResponse(400));
+
+			var ReturnedUser = new UserDto()
+			{
+				DisplayName = User.DisplayName,
+				Email = User.Email,
+				Token = "ThisWillBeToken"
+			};
+			return Ok(ReturnedUser);
+		}
+
+		
+
+	}
+}
