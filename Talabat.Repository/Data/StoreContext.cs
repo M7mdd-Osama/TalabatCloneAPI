f891a3842ed9681136799bf423d2b5ce661e3253
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-30 04:01:53 +0300

    Order Module Migration

--- Talabat.Repository/Data/StoreContext.cs
+++ Talabat.Repository/Data/StoreContext.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
 
 namespace Talabat.Repository.Data
 {
@@ -18,10 +19,13 @@ namespace Talabat.Repository.Data
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-			base.OnModelCreating(modelBuilder);
+			//base.OnModelCreating(modelBuilder);
 		}
 		public DbSet<Product> Products { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
     }
 }
